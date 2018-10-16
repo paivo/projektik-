6454d3139fc931759018fe59a725e6ed79767e1a
Pave <paveee777@gmail.com>
2018-10-16 10:48:00 +0300

    lisättiin portti

--- src/main/java/lopullinenprojekti/Main.java
+++ src/main/java/lopullinenprojekti/Main.java
@@ -21,9 +21,9 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        //if (System.getenv("PORT") != null) {
-        //   Spark.port(Integer.valueOf(System.getenv("PORT")));
-        //}
+        if (System.getenv("PORT") != null) {
+           Spark.port(Integer.valueOf(System.getenv("PORT")));
+        }
         File tiedosto = new File("db", "taulu.db");
         Database database = new Database("jdbc:sqlite:" + tiedosto.getAbsolutePath());
 

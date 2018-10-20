d8a9a0f81d61ddaa36bfa49e6e7d65b3ac459f58
Pave <paveee777@gmail.com>
2018-10-20 17:49:29 +0300

    kokeillaan luoda taulut koodissa

--- src/main/java/lopullinenprojekti/Database.java
+++ src/main/java/lopullinenprojekti/Database.java
@@ -8,6 +8,8 @@ package lopullinenprojekti;
 import java.sql.*;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Database {
 
@@ -24,5 +26,35 @@ public class Database {
         }
 
         return DriverManager.getConnection("jdbc:sqlite:taulu.db");
+    }
+    
+    public void init() {
+        List<String> lauseet = sqliteLauseet();
+
+        // "try with resources" sulkee resurssin automaattisesti lopuksi
+        try (Connection conn = getConnection()) {
+            Statement st = conn.createStatement();
+
+            // suoritetaan komennot
+            for (String lause : lauseet) {
+                System.out.println("Running command >> " + lause);
+                st.executeUpdate(lause);
+            }
+
+        } catch (Throwable t) {
+            // jos tietokantataulu on jo olemassa, ei komentoja suoriteta
+            System.out.println("Error >> " + t.getMessage());
+        }
+    }
+
+    private List<String> sqliteLauseet() {
+        ArrayList<String> lista = new ArrayList<>();
+
+        // tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä
+        lista.add("CREATE TABLE Kysymys (id SERIAL PRIMARY KEY, kurssi varchar(200), aihe varchar(200), kysymysteksti (200));");
+        lista.add("CREATE TABLE Vastaus (id SERIAL PRIMARY KEY, kysymys_id integer, vastausteksti varchar(200), oikein integer);");
+        lista.add("INSERT INTO Kysymys (kurssi, aihe, kysymysteksti) VALUES ('Lineaariset mallit', 'kovarianssimatriisi', 'Kuinka kovarianssimatriisin diagonaalialkiot kuvaavat?');");
+
+        return lista;
     }
 }
